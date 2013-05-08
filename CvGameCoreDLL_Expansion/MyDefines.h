67cd8be1d3106087dd532b50c1e96ba37826fbfb
Mark <hambil@gmail.com>
2013-05-08 09:35:02 -0700

    Added file to use for #define

---
+++ CvGameCoreDLL_Expansion/MyDefines.h
@@ -0,0 +1,18 @@
+//
+//  PURPOSE: setup defines for different code additions
+//
+#pragma once
+
+#ifndef MY_DEFINES_H
+#define MY_DEFINES_H
+
+// Comment these lines out to remove the associated code from the DLL,
+
+// Adds various missing LUA functions
+#define MOD_GLOBAL_MISSING_LUA			true
+// Adds various missing XML tags
+#define MOD_GLOBAL_MISSING_XML_TAG	    true
+// Adds various new functionality
+#define MOD_GLOBAL_NEW_FUNCTIONALITY	true
+
+#endif
